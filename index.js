5f7348cb367e1191e408fe57e1290bab7cf362ce
Alex-Harron <aharron.harron@gmail.com>
2021-06-11 16:26:25 -0400

    commit tree a3c9484b8b3358939157b5ff8c28df52ff263f1f

--- index.js
+++ index.js
@@ -8,6 +8,60 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord(row) {
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeRecord) {
+    return employeeRecord.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+function createTimeInEvent(card){
+    let [date, hour] = card.split(" ")
+
+    this.timeInEvents.push({
+        type: "TimeIn", 
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+}
+
+function createTimeOutEvent(card){
+    let [date, hour] = card.split(" ")
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+}
+
+function hoursWorkedOnDate(dateEntered){
+    let signIn = this.timeInEvents.find(function(e){
+        return e.date === dateEntered
+    })
+    let signOut = this.timeOutEvents.find(function(e){
+        return e.date === dateEntered
+    })
+    return (signOut.hour - signIn.hour) / 100
+}
+
+function wagesEarnedOnDate(dateEntered){
+    let wage = hoursWorkedOnDate.call(this, dateEntered)
+    * this.payPerHour
+    return parseFloat(wage.toString())
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
@@ -19,4 +73,16 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(function(rec){
+        return rec.firstName === firstName
+    })
+}
+
+function calculatePayroll(arrayOfEmployeeRecs){
+    return arrayOfEmployeeRecs.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
 }
